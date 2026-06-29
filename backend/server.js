bfd987efa4e75fd2aab77cdbc2efc0aeec31c1dd
anjali <s19.anjali@gmail.com>
2026-06-30 01:04:50 +0530

    fix db connection and server issues

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ const port = process.env.PORT || 4000;
 
 
 // MIDDLEWARE
-app.use(cors());
+app.use(cors({
+    origin: "*"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
